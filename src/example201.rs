cb49c5e15f61d1dc29eee94ce95c5d1bdd3f0ac0
Leo <leo@cluml.com>
2025-05-26 10:56:09 +0900

    Implement exercise 201: Concat two vectors

---
+++ src/example201.rs
@@ -0,0 +1,22 @@
+fn double_vector_i32(input: Vec<i32>) -> Vec<i32> {
+    let n = input.len();
+    let mut result = Vec::with_capacity(2 * n);
+
+    // Append the input vector twice
+    result.extend_from_slice(&input);
+    result.extend_from_slice(&input);
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let a = vec![5, 4, 3, 2, 1];
+        let doubled = double_vector_i32(a);
+        assert_eq!(doubled, vec![5, 4, 3, 2, 1, 5, 4, 3, 2, 1]);
+    }
+}
