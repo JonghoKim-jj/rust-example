7601b62c257afe973a30d34786d3b8e7ecaa5074
Leo <leo@cluml.com>
2025-05-26 09:16:15 +0900

    Implement exercise 101

---
+++ src/example101.rs
@@ -0,0 +1,33 @@
+fn add_two_integers(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let a = 99;
+        let b = 101;
+
+        assert_eq!(add_two_integers(a, b), 200);
+    }
+
+    #[test]
+    fn test2() {
+        let c = 123;
+        let d = 456;
+
+        assert_eq!(add_two_integers(c, d), 579);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test3() {
+        let e = 1;
+        let f = 2;
+
+        assert_eq!(add_two_integers(e, f), 4);
+    }
+}
