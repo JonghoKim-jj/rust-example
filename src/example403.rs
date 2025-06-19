410b3cd0e05be1e28114d244f381dc94ce20d4b9
Leo <leo@cluml.com>
2025-06-19 18:22:53 +0900

    Change example 403 to return Option<T> (#70)
    - Remove upper limit of sentence length to remove exception
    - Returns null if sentences is an empty list

--- src/example403.rs
+++ src/example403.rs
@@ -1,17 +1,8 @@
-fn solution(sentences: &[&str]) -> Result<u32, &'static str> {
-    if !(1..=100).contains(&sentences.len()) {
-        return Err("Number of sentences must be between 1 and 100");
-    }
-
-    let result: Result<u32, _> = sentences
+fn solution(sentences: &[&str]) -> Option<u32> {
+    sentences
         .iter()
-        .map(|&sentence| sentence.split_whitespace().count())
+        .filter_map(|&sentence| u32::try_from(sentence.split_whitespace().count()).ok())
         .max()
-        .expect("At least one sentence exists, thus max exists")
-        .try_into()
-        .map_err(|_| "Error converting max_words usize -> u32");
-
-    result
 }
 
 #[cfg(test)]
@@ -27,14 +18,14 @@ mod tests {
             "this is great thanks very much",
         ];
 
-        assert_eq!(solution(&sentences), Ok(6));
+        assert_eq!(solution(&sentences), Some(6));
     }
 
     #[test]
     fn test_2() {
         let sentences = ["please wait", "continue to fight", "continue to win"];
 
-        assert_eq!(solution(&sentences), Ok(3));
+        assert_eq!(solution(&sentences), Some(3));
     }
 
     #[test]
@@ -42,6 +33,13 @@ mod tests {
         use std::iter;
         let sentences: Vec<&str> = iter::repeat_n("Hello", 200).collect();
 
-        assert!(solution(&sentences).is_err());
+        assert!(solution(&sentences).is_some_and(|x| x == 1));
+    }
+
+    #[test]
+    fn test_4() {
+        let sentences: Vec<&str> = vec![];
+
+        assert!(solution(&sentences).is_none());
     }
 }
